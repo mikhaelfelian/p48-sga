4de1d66b87f095a6582b5e956f28c33271eea9b5
Redha <tataredhaalfath13@gmail.com>
2025-05-01 15:47:33 +0700

    change master template

--- app/Views/admin-lte-3/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; <?= date('Y') ?> <a href="<?= base_url() ?>"><?= $Pengaturan->judul ?></a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> <?= $Pengaturan->versi ?? '1.0.0' ?>
-    </div>
-</footer> 
--- app/Views/admin-lte-3/manajemen/laporan/data_pembelian.php
+++ app/Views/admin-lte-3/manajemen/laporan/data_pembelian.php
@@ -33,18 +33,19 @@
                             <div class="card-tools">
                             </div>
                         </div>
+                        <?php echo form_open(current_url(), ['method' => 'get']); ?>
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-6">
                                     <div class="form-group <?php echo (!empty($psnGagal['perusahaan']) ? 'text-danger' : '') ?>">
                                         <label class="control-label">Perusahaan*</label>
-                                        <select name="perusahaan" class="form-control rounded-0<?php echo (!empty($psnGagal['perusahaan']) ? ' is-invalid' : '') ?>">
+                                        <select name="filter_perusahaan" class="form-control rounded-0<?php echo (!empty($psnGagal['perusahaan']) ? ' is-invalid' : '') ?>">
                                             <option value="">- Pilih Perusahaan -</option>
                                             <?php foreach ($SQLProfile as $profile) { ?>
-                                                <option value="<?php echo $profile->id ?>"><?php echo strtoupper($profile->nama) ?></option>
+                                                <option value="<?php echo $profile->id ?>" <?php echo (request()->getVar('filter_perusahaan') == $profile->id ? 'selected' : '') ?>><?php echo strtoupper($profile->nama) ?></option>
                                             <?php } ?>
                                         </select>
-                                    </div>                                    
+                                    </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
@@ -53,7 +54,7 @@
                                             <div class="input-group-append">
                                                 <span class="input-group-text"><i class="fas fa-calendar"></i></span>
                                             </div>
-                                            <?php echo form_input(array('id' => 'tgl', 'name' => 'tgl', 'class' => 'form-control text-middle rounded-0', 'style' => 'vertical-align: middle;', 'placeholder' => '02/15/2022 ...')) ?>
+                                            <?php echo form_input(array('id' => 'tgl', 'name' => 'filter_tgl', 'class' => 'form-control text-middle rounded-0', 'style' => 'vertical-align: middle;', 'placeholder' => '02/15/2022 ...', 'value' => request()->getVar('filter_tgl'))) ?>
                                         </div>
                                     </div>
                                 </div>
@@ -66,7 +67,7 @@
                                             <div class="input-group-append">
                                                 <span class="input-group-text"><i class="fas fa-calendar"></i></span>
                                             </div>
-                                            <?php echo form_input(array('id' => 'tgl_rentang', 'name' => 'tgl_rentang', 'class' => 'form-control text-middle rounded-0', 'style' => 'vertical-align: middle;', 'placeholder' => '02/15/2022 - 02/15/2022 ...')) ?>
+                                            <?php echo form_input(array('id' => 'tgl_rentang', 'name' => 'filter_tgl_rentang', 'class' => 'form-control text-middle rounded-0', 'style' => 'vertical-align: middle;', 'placeholder' => '02/15/2022 - 02/15/2022 ...', 'value' => request()->getVar('filter_tgl_rentang'))) ?>
                                         </div>
                                     </div>
                                 </div>
@@ -76,10 +77,10 @@
                                         <select name="supplier" class="form-control rounded-0">
                                             <option value="">- Semua -</option>
                                             <?php foreach ($SQLSupplier as $supplier) { ?>
-                                                <option value="<?php echo $supplier->id ?>"><?php echo strtoupper($supplier->nama) ?></option>
+                                                <option value="<?php echo $user->id ?>" <?php echo (request()->getVar('filter_sales') == $user->id ? 'selected' : '') ?>><?php echo strtoupper($user->first_name) ?></option>
                                             <?php } ?>
                                         </select>
-                                    </div>                               
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -90,8 +91,9 @@
                                 <div class="col-lg-6 text-right">
                                     <button type="submit" class="btn btn-primary btn-flat"><i class="fa fa-search"></i> Cari</button>
                                 </div>
-                            </div> 
+                            </div>
                         </div>
+                        <?php echo form_close(); ?>
                     </div>
                 </div>
             </div>
@@ -101,7 +103,6 @@
                         <div class="card-header">
                             <h3 class="card-title">Data Pembelian</h3>
                             <div class="card-tools">
-                                <?php echo (!empty($Pagination) ? $Pagination : '') ?>
                                 <a href="<?php echo base_url('laporan/export_pembelian?' . $_SERVER['QUERY_STRING']) ?>" class="btn btn-success btn-sm">
                                     <i class="fas fa-file-excel"></i> Export Excel
                                 </a>
@@ -125,29 +126,29 @@
                                         $no = $Halaman;
                                         foreach ($SQLPembelian as $det) {
                                             $user = $ionAuth->user($det->id_user)->row();
-                                            ?>
+                                    ?>
                                             <tr>
                                                 <td style="width: 25px;" class="text-center"><?php echo $no++ ?>.</td>
                                                 <td style="width: 150px;">
-                                                    <?php echo anchor(base_url('transaksi/pembelian/data_pembelian_det.php?id=' . $det->id), $det->no_nota) ?><br/>
-                                                    <small><?php echo tgl_indo5($det->tgl_simpan) ?></small><br/>
-                                                    <small><i><?php echo !empty($user) ? strtolower($user->username) : '-' ?></i></small><br/>
+                                                    <?php echo anchor(base_url('transaksi/pembelian/data_pembelian_det.php?id=' . $det->id), $det->no_nota) ?><br />
+                                                    <small><?php echo tgl_indo5($det->tgl_simpan) ?></small><br />
+                                                    <small><i><?php echo !empty($user) ? strtolower($user->username) : '-' ?></i></small><br />
                                                 </td>
                                                 <td style="width: 450px;">
-                                                    <?php echo $det->supplier ?><br/>
-                                                    <?php echo $det->alamat ?><br/>
+                                                    <?php echo $det->supplier ?><br />
+                                                    <?php echo $det->alamat ?><br />
                                                     <?php if ($det->jml_gtotal > 0) { ?>
                                                         <small><i><b>Rp. <?php echo format_angka($det->jml_gtotal); ?></b></i></small>
                                                     <?php } ?>
                                                 </td>
-                                                <td class="text-right">
+                                                <td class="">
                                                     Rp. <?php echo format_angka($det->jml_gtotal); ?>
                                                 </td>
                                                 <td>
                                                     <?php echo status_penj($det->status) ?>
                                                 </td>
                                                 <td>
-                                                    <?php 
+                                                    <?php
                                                     if (!empty($user)) {
                                                         echo $user->first_name . ' ' . $user->last_name;
                                                     } else {
@@ -156,18 +157,30 @@
                                                     ?>
                                                 </td>
                                             </tr>
-                                            <?php
+                                        <?php
                                         }
                                     } else {
                                         ?>
                                         <tr>
                                             <td colspan="6" class="text-center">Tidak ada data pembelian</td>
                                         </tr>
-                                        <?php
+                                    <?php
                                     }
                                     ?>
                                 </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th colspan="1">Total:</th>
+                                        <th> <?php echo number_format($total_data); ?> Data</th>
+                                        <th colspan="1">Total Keseluruhan Biaya :</th>
+                                        <th colspan="3">Rp. <?php echo format_angka($total_biaya); ?></th>
+                                    </tr>
+                                </tfoot>
                             </table>
+                            <!-- FOOTER TABLE - PAGINATION -->
+                            <div class="d-flex justify-content-end mt-3">
+                                <?php echo (!empty($Pagination) ? $Pagination : ''); ?>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -178,7 +191,7 @@
     <!-- /.content -->
 </div>
 <script type="text/javascript">
-    $(function () {
+    $(function() {
         // Initialize daterangepicker for date range
         $('#tgl_rentang').daterangepicker({
             locale: {
@@ -194,7 +207,7 @@
                 format: 'MM/DD/YYYY'
             }
         });
-        
+
         <?php echo session()->getFlashdata('laporan_toast'); ?>
     });
-</script> 
+</script>

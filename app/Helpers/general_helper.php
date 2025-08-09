8bb1cdd0d0a8165edd346923be2fd414a7ee4b70
Redha <tataredhaalfath13@gmail.com>
2025-08-09 17:30:08 +0700

    enhance print invoice penjualan: auto detec nominal terbilang

--- app/Helpers/general_helper.php
+++ app/Helpers/general_helper.php
@@ -732,3 +732,41 @@ function status_karyawan($status, $rounded = true) {
     return '<span class="badge ' . $status_class . '">' . $status_label . '</span>';
 }
 
+function penyebut($nilai) {
+    $nilai = abs($nilai);
+    $huruf = ["", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
+    $temp = "";
+
+    if ($nilai < 12) {
+        $temp = $huruf[$nilai];
+    } else if ($nilai < 20) {
+        $temp = penyebut($nilai - 10) . " Belas";
+    } else if ($nilai < 100) {
+        $temp = penyebut(intval($nilai / 10)) . " Puluh " . penyebut($nilai % 10);
+    } else if ($nilai < 200) {
+        $temp = "Seratus " . penyebut($nilai - 100);
+    } else if ($nilai < 1000) {
+        $temp = penyebut(intval($nilai / 100)) . " Ratus " . penyebut($nilai % 100);
+    } else if ($nilai < 2000) {
+        $temp = "Seribu " . penyebut($nilai - 1000);
+    } else if ($nilai < 1000000) {
+        $temp = penyebut(intval($nilai / 1000)) . " Ribu " . penyebut($nilai % 1000);
+    } else if ($nilai < 1000000000) {
+        $temp = penyebut(intval($nilai / 1000000)) . " Juta " . penyebut($nilai % 1000000);
+    } else if ($nilai < 1000000000000) {
+        $temp = penyebut(intval($nilai / 1000000000)) . " Milyar " . penyebut($nilai % 1000000000);
+    } else if ($nilai < 1000000000000000) {
+        $temp = penyebut(intval($nilai / 1000000000000)) . " Triliun " . penyebut($nilai % 1000000000000);
+    }
+
+    // Hilangkan spasi berlebih
+    return trim(preg_replace('/\s+/', ' ', $temp));
+}
+
+function terbilang($nilai) {
+    if ($nilai < 0) {
+        return "Minus " . penyebut($nilai);
+    } else {
+        return penyebut($nilai);
+    }
+}
